b877c3aa41856b71be4b7744418f3eef47ac6646
CantikaTriWahyuni <149767228+cantkaptri@users.noreply.github.com>
2025-02-21 15:15:44 +0700

    praktikum minggu 3

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Profil Mahasiswa</title>
+</head>
+<body>
+    <h1>Profil Mahasiswa</h1>
+    <p><strong>Nama:</strong> Cantika Tri Wahyuni</p>
+    <p><strong>NIM:</strong> E41231450</p>
+    <p><strong>Golongan:</strong> D</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,44 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/posts', [PostController::class, 'index']);
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/hello', function () {
+return "Hello, World!";
+});
+
+// Route::get('/user/{id}', function ($id) {
+//  return "User ID: " . $id;
+//  });
+
+Route::get('/user/{name?}', function ($name = "Guest") {
+    return "Hello, " . $name;
+    });
+
+Route::get('/dashboard', function () {
+     return view('dashboard');
+    })->name('dashboard');
+
+
+Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', function () {
+    return "Admin Dashboard";
+    });
+    Route::get('/users', function () {
+    return "Admin Users";
+    });
+    });
+
+    Route::get('/link', function () {
+        return view('test');
+    });
+Route::post('/data', function () { return "POST Request"; });
+Route::put('/data', function () { return "PUT Request"; });
+Route::delete('/data', function () { return "DELETE Request"; });
+Route::patch('/data', function () { return "PATCH Request"; });
+
+
